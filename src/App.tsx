299546dbaf6b25cdbc66303d5dbb4e4a01d8bc4a
NDUBUISI OBINNA <chrisnonso8@gmail.com>
2023-09-03 15:24:10 +0100

    setup: completed font setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline font-work-sans">
-      Hello world!
+    <h1 className="text-3xl font-bold underline font-work-sans m-auto">
+      working on schoolinka task oooooo
     </h1>
   );
 }
