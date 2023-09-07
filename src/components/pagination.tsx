b9a61564e135f711b6631142c6b6953c6f074032
NDUBUISI OBINNA <chrisnonso8@gmail.com>
2023-09-07 03:33:51 +0100

    bug: fixed pagination bug

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -14,8 +14,6 @@ const Pagination: React.FC<PaginationProps> = ({
 }) => {
   const renderPageButtons = () => {
     const buttons: JSX.Element[] = [];
-
-    // Calculate the range of page buttons to render
     let start = Math.max(currentPage - 1, 1);
     let end = Math.min(start + 3, totalPages);
 
@@ -24,22 +22,28 @@ const Pagination: React.FC<PaginationProps> = ({
       end = totalPages;
     }
 
-    // Display three buttons on the left of the ellipsis
-    for (let page = start; page < currentPage; page++) {
-      buttons.push(
-        <button
-          key={page}
-          className={`h-[40px] w-[40px] transition-all ease-in-out duration-300 rounded-full text-center flex items-center justify-center cursor-pointer ${
-            page === currentPage
-              ? "bg-gray-50 font-semibold text-gray-900"
-              : "bg-transparent text-gray-600"
-          }`}
-          onClick={() => onChangePage(page)}
-        >
-          {page}
-        </button>
-      );
-    }
+    const addButtons = (start: number, end: number) => {
+      for (let page = start; page <= end; page++) {
+        buttons.push(
+          <button
+            key={page}
+            className={`h-[40px] w-[40px] transition-all ease-in-out duration-300 rounded-full text-center flex items-center justify-center cursor-pointer ${
+              page === currentPage
+                ? "bg-gray-50 font-semibold text-gray-900"
+                : "bg-transparent text-gray-600"
+            }`}
+            onClick={() => onChangePage(page)}
+          >
+            {page}
+          </button>
+        );
+      }
+    };
+
+    addButtons(
+      start,
+      currentPage + 2 < totalPages - 2 ? start + 2 : currentPage
+    );
 
     buttons.push(
       <span key="ellipsis" className="mb-4 font-bold text-2xl">
@@ -47,22 +51,10 @@ const Pagination: React.FC<PaginationProps> = ({
       </span>
     );
 
-    // Display three buttons on the right of the ellipsis
-    for (let page = currentPage + 1; page <= end; page++) {
-      buttons.push(
-        <button
-          key={page}
-          className={`h-[40px] w-[40px] transition-all ease-in-out duration-300 rounded-full text-center flex items-center justify-center cursor-pointer ${
-            page === currentPage
-              ? "bg-gray-50 font-semibold text-gray-900"
-              : "bg-transparent text-gray-600"
-          }`}
-          onClick={() => onChangePage(page)}
-        >
-          {page}
-        </button>
-      );
-    }
+    addButtons(
+      currentPage + 2 < totalPages - 2 ? end : currentPage + 1,
+      currentPage + 2 < totalPages - 2 ? end + 2 : end
+    );
 
     return buttons;
   };
@@ -70,7 +62,7 @@ const Pagination: React.FC<PaginationProps> = ({
   return (
     <div className="flex items-center justify-between">
       <button
-        className={`flex items-center justify-start gap-3 ${
+        className={`flex items-center hover:bg-gray-100 rounded-md justify-start gap-3 ${
           currentPage === 1
             ? "pointer-events-none opacity-50 cursor-not-allowed"
             : ""
@@ -83,11 +75,13 @@ const Pagination: React.FC<PaginationProps> = ({
         </span>{" "}
         <span> Previous</span>
       </button>
+
       <div className="flex items-center justify-center my-auto">
         {renderPageButtons()}
       </div>
+
       <button
-        className={` flex items-center justify-start gap-3  ${
+        className={` flex items-center hover:bg-gray-100 rounded-md justify-start gap-3  ${
           currentPage === totalPages ? "pointer-events-none opacity-50" : ""
         } px-2 py-1 ml-2`}
         onClick={() => onChangePage(currentPage + 1)}
